41424dd1e291b72cbed29593d036b41aea591733
GrantAyers22 <gayers@smu.edu>
2021-04-23 21:18:39 -0500

    Changed the creategraph.py to use global variables that can be changed much easier to test node distribution

--- python/creategraph.py
+++ python/creategraph.py
@@ -1,17 +1,108 @@
+# Imports
+import globalvariables
 import matplotlib.pyplot as plt
 import networkx as nx
+from itertools import combinations, permutations
+import random
 import sys
+import os
 
-# records command line arguments
-nodes = int(sys.argv[1])
-edges = int(sys.argv[2])
+class create_graphs:
+    def __init__(self):
 
-G = nx.grid_2d_graph(nodes, edges)  # 5x5 grid
+        # Creating the Global Variables Item
+        self.Global = globalvariables.global_variables()
 
-# print the adjacency list
-for line in nx.generate_adjlist(G):
-    print(line)
-# write edgelist to input file
-nx.write_edgelist(G, path="../data/input.txt", delimiter=":")
-# read edgelist from input file
-H = nx.read_edgelist(path="../data/input.txt", delimiter=":")
+        # Creating the list to hold the graphs
+        self.Graphs = list()
+
+        for i in range(0, len(self.Global.get_nodes())):
+            self.Graphs.append(self.generate_graph(self.Global.get_nodes()[i], self.Global.get_densities()[i]))
+
+        i = 0
+        for graph in self.Graphs:
+            nx.draw(graph)
+            plt.savefig("../Data/graph" + str(i) + ".png")
+            i += 1
+        
+        self.generate_graph_data()
+
+
+    def generate_graph(self, number_of_nodes, density):
+        # Creating the list of nodes
+        nodes = list(range(0, number_of_nodes))
+
+        # Creates a list of all the possible edges or 100% Density
+        possible_edges = list(combinations(nodes, 2))
+        total_possible_edges = len(possible_edges)
+        
+        # Creating the list iterator
+        i = 0
+
+        # Creating the Graph object to return
+        G = self.create_least_connected_graph(nodes)
+
+        # This is a loop to remove all the edges until the density is correct
+        while True:
+
+            # Calculating the density using number of current edges/total
+            achieved_density = float(G.number_of_edges()/total_possible_edges)
+            
+            # Checking if the graph is at the proper density
+            if achieved_density >= float(density/100):
+                print("Total possible Edges: " + str(total_possible_edges) + 
+                      "\nNumber of Edges: " + str(G.number_of_edges()) + 
+                      "\nActual Density: " + str(float(100*(G.number_of_edges())/total_possible_edges)) + "\n" )
+                return G
+
+            if i == len(possible_edges)-1:
+                i = 0
+
+            # Generating the random chance of the edge being added to get proper density
+            r = random.randint(0, 100)
+
+            # Check if this edge is added
+            if r < density:
+                G.add_edge(possible_edges[i][0], possible_edges[i][1])
+                possible_edges.remove(possible_edges[i])
+                i-=1
+            
+            i+=1        
+        return G
+
+    def create_least_connected_graph(self, nodes):
+        G = nx.Graph()
+        i = 0
+        while i != len(nodes)-1:
+            print(str(i) + ", " + str(i+1))
+            G.add_edge(i, i+1)
+            i+=1
+        
+        return G
+    
+
+    def generate_graph_data(self):
+        i = 0
+        for graph in self.Graphs:
+            with open("../data/graph" + str(i) + ".txt", "w") as file:
+                output = str(graph.edges())
+                output = output.replace(", (", "\n(")
+                output = output.replace('[', '')
+                output = output.replace(']', '')
+                file.write(str(graph.number_of_edges())+ "\n")
+                file.write(output)
+            i+=1
+
+
+if __name__ == "__main__":
+    c = create_graphs()
+
+
+
+
+
+
+
+
+
+
---
+++ python/globalvariables.py
@@ -0,0 +1,45 @@
+# Imports
+import matplotlib.pyplot as plt
+import networkx as nx
+import sys
+import os
+
+class global_variables:
+    def __init__(self):
+        # Declaring the list that will store the values for nodes and edges
+        self.nodes = list()
+        self.densities = list()
+
+        # Getting the location of the current python file
+        # This is useful for Relative-Pathing later
+        current_absolute_path = os.path.dirname(__file__)
+
+        # This is the relative path from the python file to the global variable file
+        global_variable_relative_path = "../Data/Global-Variables.txt"
+
+        # This is the absolute path from the python file to the global variable file
+        global_variable_absolute_path = os.path.join(current_absolute_path, global_variable_relative_path)
+
+        # Reading the global variables into a variable called data and splitting it into an array by line
+        with open(global_variable_absolute_path, 'r') as file:
+            data = file.read().split('\n')
+
+        # Looping through all the variables in the global variables
+        for variable in data:
+
+            # If this data pertains to Graph-
+            if 'Graph-' in variable:
+                
+                # If the data is a Graph-Node we will append its value to the node list
+                if '-Nodes' in variable:
+                    self.nodes.append(int(variable[variable.index(' '): len(variable)]))
+                
+                # If the data is a Graph-Node we will append its value to the node list
+                if '-Density' in variable:
+                    self.densities.append(int(variable[variable.index(' '): len(variable)]))
+
+    def get_nodes(self):
+        return self.nodes
+
+    def get_densities(self): 
+        return self.densities
